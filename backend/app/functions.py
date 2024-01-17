aee3df986a065e758c2021527ebcbfc4ea9a9f48
tajay59 <tajay59@gmail.com>
2024-01-17 14:01:30 -0500

    Initial Upload

---
+++ backend/app/functions.py
@@ -0,0 +1,135 @@
+ #!/usr/bin/python3
+
+
+#################################################################################################################################################
+#                                                    CLASSES CONTAINING ALL THE APP FUNCTIONS                                                                                                    #
+#################################################################################################################################################
+
+
+class DB:
+
+    def __init__(self,Config):
+
+        from math import floor
+        from os import getcwd
+        from os.path import join
+        from json import loads, dumps, dump
+        from datetime import timedelta, datetime, timezone 
+        from pymongo import MongoClient , errors, ReturnDocument
+        from urllib import parse
+        from urllib.request import  urlopen 
+        from bson.objectid import ObjectId  
+       
+      
+        self.Config                         = Config
+        self.getcwd                         = getcwd
+        self.join                           = join 
+        self.floor                      	= floor 
+        self.loads                      	= loads
+        self.dumps                      	= dumps
+        self.dump                       	= dump  
+        self.datetime                       = datetime
+        self.ObjectId                       = ObjectId 
+        self.server			                = Config.DB_SERVER
+        self.port			                = Config.DB_PORT
+        self.username                   	= parse.quote_plus(Config.DB_USERNAME)
+        self.password                   	= parse.quote_plus(Config.DB_PASSWORD)
+        self.remoteMongo                	= MongoClient
+        self.ReturnDocument                 = ReturnDocument
+        self.PyMongoError               	= errors.PyMongoError
+        self.BulkWriteError             	= errors.BulkWriteError  
+        self.tls                            = False # MUST SET TO TRUE IN PRODUCTION
+
+
+    def __del__(self):
+            # Delete class instance to free resources
+            pass
+ 
+
+
+    ####################
+    # LAB 2 FUNCTIONS  #
+    ####################
+    
+    def addUpdate(self,data):
+        '''ADD A NEW STORAGE LOCATION TO COLLECTION'''
+        try:
+            remotedb 	= self.remoteMongo('mongodb://%s:%s@%s:%s' % (self.username, self.password,self.server,self.port), tls=self.tls)
+            result      = remotedb.ELET2415.climo.insert_one(data)
+        except Exception as e:
+            msg = str(e)
+            if "duplicate" not in msg:
+                print("addUpdate error ",msg)
+            return False
+        else:                  
+            return True
+        
+       
+
+    def getAllInRange(self,start, end):
+        '''RETURNS A LIST OF OBJECTS. THAT FALLS WITHIN THE START AND END DATE RANGE'''
+        try:
+            remotedb 	= self.remoteMongo('mongodb://%s:%s@%s:%s' % (self.username, self.password,self.server,self.port), tls=self.tls)
+            result      = list(remotedb.ELET2415.climo.find('''Add your query here'''))
+        except Exception as e:
+            msg = str(e)
+            print("getAllInRange error ",msg)            
+        else:                  
+            return result
+        
+
+    def humidityMMAR(self,start, end):
+        '''RETURNS MIN, MAX, AVG AND RANGE FOR HUMIDITY. THAT FALLS WITHIN THE START AND END DATE RANGE'''
+        try:
+            remotedb 	= self.remoteMongo('mongodb://%s:%s@%s:%s' % (self.username, self.password,self.server,self.port), tls=self.tls)
+            result      = list(remotedb.ELET2415.climo.aggregate( '''Add your Aggregation pipeline here in this function'''))
+        except Exception as e:
+            msg = str(e)
+            print("humidityMMAS error ",msg)            
+        else:                  
+            return result
+        
+    def temperatureMMAR(self,start, end):
+        '''RETURNS MIN, MAX, AVG AND RANGE FOR TEMPERATURE. THAT FALLS WITHIN THE START AND END DATE RANGE'''
+        try:
+            remotedb 	= self.remoteMongo('mongodb://%s:%s@%s:%s' % (self.username, self.password,self.server,self.port), tls=self.tls)
+            result      = list(remotedb.ELET2415.climo.aggregate( '''Add your Aggregation pipeline here in this function'''))
+        except Exception as e:
+            msg = str(e)
+            print("temperatureMMAS error ",msg)            
+        else:                  
+            return result
+
+
+    def frequencyDistro(self,variable,start, end):
+        '''RETURNS THE FREQUENCY DISTROBUTION FOR A SPECIFIED VARIABLE WITHIN THE START AND END DATE RANGE'''
+        try:
+            remotedb 	= self.remoteMongo('mongodb://%s:%s@%s:%s' % (self.username, self.password,self.server,self.port), tls=self.tls)
+            result      = list(remotedb.ELET2415.climo.aggregate( '''Add your Aggregation pipeline here in this function'''))
+        except Exception as e:
+            msg = str(e)
+            print("frequencyDistro error ",msg)            
+        else:                  
+            return result
+        
+ 
+
+
+
+def main():
+    from config import Config
+    from time import time, ctime, sleep
+    from math import floor
+    from datetime import datetime, timedelta
+    one = DB(Config)
+ 
+ 
+    start = time() 
+    end = time()
+    print(f"completed in: {end - start} seconds")
+    
+if __name__ == '__main__':
+    main()
+
+
+    
---
+++ hardware/hardware.ino
@@ -0,0 +1,257 @@
+//##################################################################################################################
+//##                                      ELET2415 DATA ACQUISITION SYSTEM CODE                                   ##
+//##                                                                                                              ##
+//##################################################################################################################
+
+// LIBRARY IMPORTS
+#include <rom/rtc.h> 
+#include <math.h>  // https://www.tutorialspoint.com/c_standard_library/math_h.htm 
+#include <ctype.h>
+
+// ADD YOUR IMPORTS HERE
+
+
+
+#ifndef _WIFI_H 
+#include <WiFi.h>
+#endif
+
+#ifndef STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifndef STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef ARDUINO_H
+#include <Arduino.h>
+#endif 
+ 
+#ifndef ARDUINOJSON_H
+#include <ArduinoJson.h>
+#endif
+
+ 
+
+// DEFINE VARIABLES
+#define ARDUINOJSON_USE_DOUBLE      1 
+// DEFINE THE CONTROL PINS FOR BOTH LEDs and A POTENTIOMETER
+
+
+
+
+// MQTT CLIENT CONFIG  
+static const char* pubtopic      = "620012345";                    // Add your ID number here
+static const char* subtopic[]    = {"620012345_sub","/elet2415"};  // Array of Topics(Strings) to subscribe to
+static const char* mqtt_server   = "local";         // Broker IP address or Domain name as a String 
+static uint16_t mqtt_port        = 1883;
+
+// WIFI CREDENTIALS
+const char* ssid       = "YOUR_SSID";     // Add your Wi-Fi ssid
+const char* password   = "YOUR_PASSWORD"; // Add your Wi-Fi password 
+
+
+
+
+// TASK HANDLES 
+TaskHandle_t xMQTT_Connect          = NULL; 
+TaskHandle_t xNTPHandle             = NULL;  
+TaskHandle_t xLOOPHandle            = NULL;  
+TaskHandle_t xUpdateHandle          = NULL;
+TaskHandle_t xButtonCheckeHandle    = NULL;  
+
+// FUNCTION DECLARATION   
+void checkHEAP(const char* Name);   // RETURN REMAINING HEAP SIZE FOR A TASK
+void initMQTT(void);                // CONFIG AND INITIALIZE MQTT PROTOCOL
+unsigned long getTimeStamp(void);   // GET 10 DIGIT TIMESTAMP FOR CURRENT TIME
+void callback(char* topic, byte* payload, unsigned int length);
+void initialize(void);
+bool publish(const char *topic, const char *payload); // PUBLISH MQTT MESSAGE(PAYLOAD) TO A TOPIC
+void vButtonCheck( void * pvParameters );
+void vUpdate( void * pvParameters );  
+bool isNumber(double number);
+ 
+
+/* Declare your functions below */ 
+double convert_Celsius_to_fahrenheit(double c);
+double convert_fahrenheit_to_Celsius(double f);
+double calcHeatIndex(double Temp, double Humid);
+
+
+/* Init class Instances for the DHT22 etcc */
+ 
+  
+
+//############### IMPORT HEADER FILES ##################
+#ifndef NTP_H
+#include "NTP.h"
+#endif
+
+#ifndef MQTT_H
+#include "mqtt.h"
+#endif
+
+// Temporary Variables 
+
+
+void setup() {
+  Serial.begin(115200);  // INIT SERIAL  
+
+  // CONFIGURE THE ARDUINO FOR ALL SENSORS AND DEVICES
+
+  initialize();     // INIT WIFI, MQTT & NTP 
+  // vButtonCheckFunction(); // UNCOMMENT IF USING BUTTONS INT THIS LAB, THEN ADD LOGIC FOR INTERFACING WITH BUTTONS IN THE vButtonCheck FUNCTION
+ }
+  
+
+
+void loop() {
+    // put your main code here, to run repeatedly:       
+    vTaskDelay(1000 / portTICK_PERIOD_MS);    
+}
+
+
+
+
+  
+//####################################################################
+//#                          UTIL FUNCTIONS                          #       
+//####################################################################
+void vButtonCheck( void * pvParameters )  {
+    configASSERT( ( ( uint32_t ) pvParameters ) == 1 );     
+      
+    for( ;; ) {
+        // Add code here to check if a button(S) is pressed
+        // then execute appropriate function if a button is pressed  
+
+        vTaskDelay(200 / portTICK_PERIOD_MS);  
+    }
+}
+
+void vUpdate( void * pvParameters )  {
+    configASSERT( ( ( uint32_t ) pvParameters ) == 1 );    
+           
+    for( ;; ) {
+          // #######################################################
+          // ## This function must PUBLISH to topic every second. ##
+          // #######################################################
+   
+          // 1. Read Humidity (the default) and save in variable below
+          double h = 0;
+           
+          // 2. Read temperature as Celsius (the default) and save in variable below
+          double t = 0;    
+ 
+
+          if(isNumber(t)){
+              // ##Publish update according to ‘{"id": "student_id", "timestamp": 1702212234, "temperature": 30, "humidity":90, "heatindex": 30}’
+
+              // 1. Create JSon object
+              
+              // 2. Create message array
+              
+              // 3. Add key:value pairs to JSon object based on above schema
+
+              // 4. Seralize / Covert JSon object to JSon string and store in message array
+               
+              // 5. Publish message to a topic sobscribed to by both backend and frontend                
+
+          }
+
+          
+            
+        vTaskDelay(1000 / portTICK_PERIOD_MS);  
+    }
+}
+
+ 
+
+unsigned long getTimeStamp(void) {
+          // RETURNS 10 DIGIT TIMESTAMP REPRESENTING CURRENT TIME
+          time_t now;         
+          time(&now); // Retrieve time[Timestamp] from system and save to &now variable
+          return now;
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // ############## MQTT CALLBACK  ######################################
+  // RUNS WHENEVER A MESSAGE IS RECEIVED ON A TOPIC SUBSCRIBED TO
+  
+  Serial.printf("\nMessage received : ( topic: %s ) \n",topic ); 
+  char *received = new char[length + 1] {0}; 
+  
+  for (int i = 0; i < length; i++) { 
+    received[i] = (char)payload[i];    
+  }
+
+  // PRINT RECEIVED MESSAGE
+  Serial.printf("Payload : %s \n",received);
+
+ 
+  // CONVERT MESSAGE TO JSON
+  StaticJsonDocument<1000> doc;
+  DeserializationError error = deserializeJson(doc, received);  
+
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+
+
+  // PROCESS MESSAGE
+  const char* type = doc["type"]; 
+
+  if (strcmp(type, "controls") == 0){
+    // 1. EXTRACT ALL PARAMETERS: NODES, RED,GREEN, BLUE, AND BRIGHTNESS FROM JSON OBJECT
+
+    // 2. ITERATIVELY, TURN ON LED(s) BASED ON THE VALUE OF NODES. Ex IF NODES = 2, TURN ON 2 LED(s)
+
+    // 3. ITERATIVELY, TURN OFF ALL REMAINING LED(s).
+   
+  }
+}
+
+bool publish(const char *topic, const char *payload){   
+     bool res = false;
+     try{
+        res = mqtt.publish(topic,payload);
+        // Serial.printf("\nres : %d\n",res);
+        if(!res){
+          res = false;
+          throw false;
+        }
+     }
+     catch(...){
+      Serial.printf("\nError (%d) >> Unable to publish message\n", res);
+     }
+  return res;
+}
+
+
+
+//***** Complete the util functions below ******
+
+double convert_Celsius_to_fahrenheit(double c){    
+    // CONVERTS INPUT FROM °C TO °F. RETURN RESULTS     
+}
+
+double convert_fahrenheit_to_Celsius(double f){    
+    // CONVERTS INPUT FROM °F TO °C. RETURN RESULT    
+}
+
+double calcHeatIndex(double Temp, double Humid){
+    // CALCULATE AND RETURN HEAT INDEX USING EQUATION FOUND AT
+    // https://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml 
+}
+ 
+
+bool isNumber(double number){       
+        char item[20];
+        snprintf(item, sizeof(item), "%f\n", number);
+        if( isdigit(item[0]) )
+          return true;
+        return false; 
+} 
