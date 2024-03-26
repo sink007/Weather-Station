45b7dce02b1ac01117304b1898d08aea89d47b52
sink007 <lhines2000@gmail.com>
2024-03-25 23:15:20 -0500

    hardware complete

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -8,10 +8,11 @@
 #include <math.h>  // https://www.tutorialspoint.com/c_standard_library/math_h.htm 
 #include <ctype.h>
 
-// ADD YOUR IMPORTS HER
+// IMPORTS
 #include <DHT.h>
-#include <FastLED.h>
-
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+#include <Adafruit_BMP280.h>
 
 #ifndef _WIFI_H 
 #include <WiFi.h>
@@ -33,31 +34,34 @@
 #include <ArduinoJson.h>
 #endif
 
- 
-
 // DEFINE VARIABLES
 #define ARDUINOJSON_USE_DOUBLE      1 
 
-// DEFINE THE CONTROL PINS FOR THE DHT22 
 #define DHT_pin 15
 #define DHTTYPE DHT22 
-#define LED_pin 22
+#define soil 36
+#define TFT_DC    17
+#define TFT_CS    5
+#define TFT_RST   16
+#define TFT_CLK   18
+#define TFT_MOSI  23
+#define TFT_MISO  19
 
-#define NUM_LEDS 7
-
+#include <Fonts/FreeSansBold18pt7b.h>
+#include <Fonts/FreeSansBold9pt7b.h> 
+#include <Fonts/FreeSansBold12pt7b.h>
+#include <Fonts/FreeSans9pt7b.h> 
 
 
 // MQTT CLIENT CONFIG  
 static const char* pubtopic      = "620155784";                    // Add your ID number here
 static const char* subtopic[]    = {"620155784_sub","/elet2415"};  // Array of Topics(Strings) to subscribe to
-static const char* mqtt_server   = "www.yanacreations.com";         // Broker IP address or Domain name as a String 
+static const char* mqtt_server   = "broker.hivemq.com";         // Broker IP address or Domain name as a String 
 static uint16_t mqtt_port        = 1883;
 
 // WIFI CREDENTIALS
-const char* ssid       = "MonaConnect";     // Add your Wi-Fi ssid
-const char* password   = ""; // Add your Wi-Fi password 
-
-
+const char* ssid       = "Hello";     // Add your Wi-Fi ssid
+const char* password   = "family123"; // Add your Wi-Fi password 
 
 
 // TASK HANDLES 
@@ -76,7 +80,6 @@ void initialize(void);
 bool publish(const char *topic, const char *payload); // PUBLISH MQTT MESSAGE(PAYLOAD) TO A TOPIC
 void vButtonCheck( void * pvParameters );
 void vUpdate( void * pvParameters );  
-bool isNumber(double number);
  
 
 /* Declare your functions below */ 
@@ -85,9 +88,11 @@ double convert_fahrenheit_to_Celsius(double f);
 double calcHeatIndex(double Temp, double Humid);
 
 
-/* Init class Instances for the DHT22 etcc */
+/* Init class Instances*/
 DHT dht(DHT_pin, DHTTYPE);
-  
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+Adafruit_BMP280 bmp; 
+
 
 //############### IMPORT HEADER FILES ##################
 #ifndef NTP_H
@@ -99,9 +104,14 @@ DHT dht(DHT_pin, DHTTYPE);
 #endif
 
 // Temporary Variables 
-float hum;  //Stores humidity value
-float celsius; //Stores temperature value cel
-float far;  // fahrenheit
+double value; //analog value from soil sensor
+double soilMoisture; //soil mositure %
+double hum; //humidity
+double temp; 
+double far; //temp in fahrenheit
+double heatindex; 
+double pressure; 
+double alt; //altitude
 const double c1 = -42.379;
 const double c2 = 2.04901523;
 const double c3 = 10.14333127;
@@ -111,16 +121,45 @@ const double c6 = -5.481717e-02;
 const double c7 = 1.22874e-03;
 const double c8 = 8.5282e-04;
 const double c9 = -1.99e-06;
-CRGB leds[NUM_LEDS];
+const int dry = 3600; // value for air
+const int wet = 1600; // value for water
+
   
 void setup() {
   Serial.begin(115200);  // INIT SERIAL  
-
-  // INITIALIZE ALL SENSORS AND DEVICES
-  
+  bmp.begin(0x76);
   dht.begin();
-  /* Add all other necessary sensor Initializations and Configurations here */
-  FastLED.addLeds<NEOPIXEL, LED_pin>(leds, NUM_LEDS);
+  tft.begin();
+  tft.setRotation(3); // Rotate the display to default orientation
+  tft.fillScreen(ILI9341_BLACK); // Clear the screen
+
+   // Set up the title and background
+  tft.setFont(&FreeSansBold12pt7b);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setCursor(75, 35); 
+  tft.println("Weather Station");
+  tft.setFont(&FreeSansBold9pt7b);
+  tft.setCursor(20, 65);
+  tft.println("Done by: L.Hines");
+
+  tft.drawRect(20, 85, 285, 150, ILI9341_WHITE); // Shifted up by 15 pixels
+
+  // Set up the text for displaying sensor values
+  tft.setFont(&FreeSans9pt7b);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setCursor(30, 113); 
+  tft.print("Temperature: ");
+  tft.setCursor(30, 133); 
+  tft.print("Humidity: ");
+  tft.setCursor(30, 153);
+  tft.print("Heat Index: ");
+  tft.setCursor(30, 173); 
+  tft.print("Soil Moisture: ");
+  tft.setCursor(30, 193); 
+  tft.print("Pressure: ");
+  tft.setCursor(30, 213); 
+  tft.print("Altitude: ");
+
   initialize();     // INIT WIFI, MQTT & NTP 
   //vButtonCheckFunction(); // UNCOMMENT IF USING BUTTONS INT THIS LAB, THEN ADD LOGIC FOR INTERFACING WITH BUTTONS IN THE vButtonCheck FUNCTION
  }
@@ -158,41 +197,70 @@ void vUpdate( void * pvParameters )  {
           // ## This function must PUBLISH to topic every second. ##
           // #######################################################
    
-          // 1. Read Humidity and save in variable below
-          double h = 0;
-          h= dht.readHumidity();
-          // 2. Read temperature as Celsius   and save in variable below
-          double t = 0;    
-          t= dht.readTemperature();
+          value = analogRead(soil);
+          soilMoisture = map(value,dry,wet,0,100);
+          if (soilMoisture>100){
+            soilMoisture= 100;
+          }
+          else if(soilMoisture<0){
+            soilMoisture=0;
+          }
+          hum = dht.readHumidity();
+          temp = dht.readTemperature();
+          far = convert_Celsius_to_fahrenheit(temp);
+          heatindex = convert_fahrenheit_to_Celsius(calcHeatIndex(far,hum)); 
+          pressure= bmp.readPressure()/1000;
+          alt= bmp.readAltitude(1012.3);
+          
+          tft.fillRect(150, 90, 151, 140, ILI9341_BLACK); // Clear the box 
+          tft.setCursor(150, 113); 
+          tft.print(temp);
+          tft.println(" C");
+          tft.setCursor(150, 133); 
+          tft.print(hum);
+          tft.println(" %");
+          tft.setCursor(150, 153); 
+          tft.print(heatindex);
+          tft.println(" C");
+          tft.setCursor(150, 173); 
+          tft.print(soilMoisture);
+          tft.println(" %");
+          tft.setCursor(150, 193); 
+          tft.print(pressure);
+          tft.println(" kPa");
+          tft.setCursor(150, 213); 
+          tft.print(alt);
+          tft.println(" m");
+          
+          // ##Publish update according to ‘{"id": "student_id", "timestamp": 1702212234, "temperature": 30, "humidity":90, "heatindex": 30, "pressure":30, "altitude": 30, "soilMoisture": 30}’
+          // 1. Create JSon object
+          StaticJsonDocument<1000> doc; // Create JSon object
+          
+          // 2. Create message buffer/array to store serialized JSON object
+          char message[900]  = {0};
 
-          double f=convert_Celsius_to_fahrenheit(t);
-       
-          if(isNumber(t)){
-              // ##Publish update according to ‘{"id": "student_id", "timestamp": 1702212234, "temperature": 30, "humidity":90, "heatindex": 30}’
+          // 3. Add key:value pairs to JSon object based on above schema
+          doc["id"]         = "620155784"; // Change to your student ID number
+          doc["timestamp"]  = getTimeStamp();
+          doc["temperature"]= temp;
+          doc["humidity"]= hum;
+          doc["heatindex"]= heatindex;
+          doc["pressure"]= pressure; 
+          doc["altitude"]= alt;
+          doc["soilMoisture"]= soilMoisture; 
 
-              // 1. Create JSon object
-              StaticJsonDocument<768> doc; // Create JSon object
-              
-              // 2. Create message buffer/array to store serialized JSON object
-              char message[800]  = {0};
+          // 4. Seralize / Covert JSon object to JSon string and store in message array
+          serializeJson(doc, message);
+        
+          // 5. Publish message to a topic sobscribed to by both backend and frontend                
+          publish(pubtopic, message);
 
-              // 3. Add key:value pairs to JSon object based on above schema
-              doc["id"]         = "620155784"; // Change to your student ID number
-              doc["timestamp"]  = getTimeStamp();
-              doc["temperature"]= t;
-              doc["humidity"]= h;
-              doc["heatindex"]= convert_fahrenheit_to_Celsius(calcHeatIndex(f,h)); 
-              // 4. Seralize / Covert JSon object to JSon string and store in message array
-              serializeJson(doc, message);
-              // 5. Publish message to a topic sobscribed to by both backend and frontend                
-              publish(pubtopic, message);
+          vTaskDelay(2000 / portTICK_PERIOD_MS);  
           }
 
-          
-            
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  
-    }
-}
+    
+  }
+
 
  
 
@@ -233,31 +301,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
   // PROCESS MESSAGE
   const char* type = doc["type"]; 
 
-  if (strcmp(type, "controls") == 0){
-    // 1. EXTRACT ALL PARAMETERS: NODES, RED,GREEN, BLUE, AND BRIGHTNESS FROM JSON OBJECT
-    int nodes = doc["leds"];
-    const char* color = doc["color"];
-    uint8_t  green = doc["color"]["g"];
-    uint8_t  blue = doc["color"]["b"];
-    uint8_t  red = doc["color"]["r"];
-    uint8_t  brightness = doc["brightness"];
-    
-    // 2. ITERATIVELY, TURN ON LED(s) BASED ON THE VALUE OF NODES. Ex IF NODES = 2, TURN ON 2 LED(s)
-    for (int i = 0; i < nodes; ++i) {
-       leds[i] = CRGB(red, green, blue);
-       FastLED.setBrightness(brightness);
-       FastLED.show();
-       delay(50);
-    }
-    // 3. ITERATIVELY, TURN OFF ALL REMAINING LED(s).
-    for (int i = nodes; i < NUM_LEDS; ++i) {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-    }
- 
- }
-
 }
 
 bool publish(const char *topic, const char *payload){   
@@ -275,7 +318,6 @@ bool publish(const char *topic, const char *payload){
      }
   return res;
 }
-
 
 //***** Complete the util functions below ******
 
@@ -296,11 +338,3 @@ double calcHeatIndex(double T, double H){
   //return dht.computeHeatIndex(T, H, false);
 }
  
-
-bool isNumber(double number){       
-        char item[20];
-        snprintf(item, sizeof(item), "%f\n", number);
-        if( isdigit(item[0]) )
-          return true;
-        return false; 
-} 
